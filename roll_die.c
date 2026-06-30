fc28c1fbc1f494395efadd12b7db0344ecc1a2e9
jw-underwood-47 <jwunderwood47@gmail.com>
2026-06-29 22:01:39 -0400

    create .gitignore add check to verify argument to roll_die

--- roll_die.c
+++ roll_die.c
@@ -4,7 +4,10 @@
 
 int main(int argc, char*argv[]){
     srand(time(NULL));
-    if (argc == 2){
-        printf("You rolled a %d\n", (rand()%(atoi(argv[1])))+1);
+    switch(argc){
+        case 0:
+        case 1: printf("error: expected at least one argument, got none\n");
+                exit(-1);
+        case 2: printf("You rolled a %d\n", (rand()%(atoi(argv[1])))+1);
     }
 }
